720692c90a89c95429ba4c5e11ab637128ee1d2f
McLarenCollege <hello@mclarencollege.in>
2019-10-07 13:26:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: TranslateApp(),
+    ));
+
+class TranslateApp extends StatefulWidget {
+  @override
+  _TranslateAppState createState() => _TranslateAppState();
+}
+
+class _TranslateAppState extends State<TranslateApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
